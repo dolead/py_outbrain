75bbd4459a20c3493fb9884803545c05b4215fd8
Ioanna Giannopoulou <joanna.gian@gmail.com>
2018-05-28 18:15:53 +0200

    PeriodicReport

--- py_outbrain/services/__init__.py
+++ py_outbrain/services/__init__.py
@@ -1,2 +1,2 @@
 from py_outbrain.services.service import CampaignService, AccountService
-from py_outbrain.services.report import CampaignSummaryReport
+from py_outbrain.services.report import PeriodicReport
