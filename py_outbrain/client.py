6e7fcb45f01eb80f62d7e2a0904290b8897ccc89
ioannagian <ioannagian@users.noreply.github.com>
2018-07-17 14:51:03 +0200

    Added TooManyAuthRequests error (#1)
    * Change management of access_token:
    
    * Can now add a token_getter, token_setter method to client to store the
    current access token.
    
    * Added TooManyAuthRequests error

--- py_outbrain/client.py
+++ py_outbrain/client.py
@@ -1,30 +1,53 @@
 import json
 import logging
 import requests
+import requests.auth
 
-from py_outbrain.errors import Unauthorized
+from py_outbrain.errors import Unauthorized, TooManyRequests, \
+    TooManyAuthRequests
 from py_outbrain.utils import parse_response
 
 logger = logging.getLogger(__name__)
 
+MOCK_URL = 'https://private-anon-832f2f406c-amplifyv01.apiary-mock.com'
+PROD_URL = 'https://api.outbrain.com/amplify/v0.1'
+
 
 class OutbrainClient:
-    def __init__(self, access_token, username, password, base_url=None):
-        self.base_url = base_url or 'https://api.outbrain.com/amplify/v0.1'
+
+    default_access_token = None
+
+    def __init__(self, username, password, dry_run=False,
+                 token_getter=None, token_setter=None):
+        if dry_run:
+            self.base_url = MOCK_URL
+        else:
+            self.base_url = PROD_URL
         self.username = username
         self.password = password
 
-        if not access_token:
-            access_token = self.generate_new_token(username, password)
-        self.access_token = access_token
+        def _default_token_setter(token):
+            self.default_access_token = token
 
-    def generate_new_token(self, username, password):
+        def _default_token_getter():
+            return self.default_access_token
+
+        self._token_getter = token_getter or _default_token_getter
+        self._token_setter = token_setter or _default_token_setter
+
+    def generate_new_token(self):
         login_url = '{}/login'.format(self.base_url)
 
-        basic_auth = requests.auth.HTTPBasicAuth(username, password)
-        r = requests.get(login_url, auth=basic_auth)
+        basic_auth = requests.auth.HTTPBasicAuth(self.username, self.password)
+        try:
+            r = requests.get(login_url, auth=basic_auth)
+            parse_response(r)
+        except TooManyRequests:
+            raise TooManyAuthRequests('Rate limit reached for token requests.')
         results = json.loads(r.text)
-        return results['OB-TOKEN-V1']
+        token = results['OB-TOKEN-V1']
+        self.access_token = token
+        return token
 
     @property
     def authorization_header(self):
@@ -33,6 +56,17 @@ class OutbrainClient:
         return {
             'OB-TOKEN-V1': self.access_token
         }
+
+    @property
+    def access_token(self):
+        token = self._token_getter()
+        if not token:
+            token = self.generate_new_token()
+        return token
+
+    @access_token.setter
+    def access_token(self, access_token):
+        self._token_setter(access_token)
 
     def execute(self, method, uri, query_params=None,
                 allow_refresh=True, raw=False, authenticated=True,
@@ -53,6 +87,6 @@ class OutbrainClient:
         except Unauthorized:
             if not allow_refresh:
                 raise
-            self.generate_new_token(self.username, self.password)
-            return self.execute(method, uri, query_params=query_params,
-                                raw=raw, allow_refresh=False, **payload)
+            if self.generate_new_token():
+                return self.execute(method, uri, query_params=query_params,
+                                    raw=raw, allow_refresh=False, **payload)
