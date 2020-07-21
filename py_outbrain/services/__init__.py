0b8f9b6f804aa6ac4c1ee2afbeae2abcb7aef63e
Emilien Rondeleux <emilien.rondeleux@dolead.com>
2020-07-21 10:14:41 +0200

    wip

--- py_outbrain/services/__init__.py
+++ py_outbrain/services/__init__.py
@@ -1,2 +1,3 @@
-from py_outbrain.services.service import CampaignService, AccountService
+from py_outbrain.services.service import (CampaignService, AccountService,
+                                          PromotedLinkService)
 from py_outbrain.services.report import PeriodicReport
